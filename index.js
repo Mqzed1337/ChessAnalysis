63e5a9a540342f1b6d667ac74d2a412f81245f91
Mqzed1337 <153781300+Mqzed1337@users.noreply.github.com>
2024-02-19 10:20:14 +1030

    test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+const { EventEmitter } = require('events');
+const EventEmitter = new EventEmitter();
+
+eventEmitter.on('start', () => {
+
+    console.log('started');
+});
+
+globalThis.yes = '23'
+
+console.log(globalThis.yes)
